f2c403e5f0686f2e916a854ccabced12061baa21
maggie.jiayan <jl7354@nyu.edu>
2016-07-13 18:16:53 -0400

    seesion5-inclasssync
    fresh copy from ect2200

---
+++ session3/src/fibber.js
@@ -0,0 +1,26 @@
+
+//var is used to create variables
+//using the {} to create a variable that is type object
+var fibber = {};
+
+fibber.init = function(){
+
+};
+
+fibber.selectChoice
+
+
+fibber.endGame = function(){
+
+};
+
+      $(document).ready(function(){
+            $("#startbtn").mouseenter(function(){
+            	$("#startbtn").fadeTo("fast", 0.5);
+            });
+
+            $("#startbtn").mouseleave(function(){
+                $("#startbtn").fadeTo("fast", 1);
+             }); 
+        });
+
